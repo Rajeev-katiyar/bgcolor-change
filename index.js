bdbf5f2aec380889172dc8f5fab69c5c808e0556
Rajeev-katiyar <138558221+Rajeev-katiyar@users.noreply.github.com>
2023-08-07 14:10:49 +0530

    bg color

---
+++ index.js
@@ -0,0 +1,27 @@
+
+function red(){
+    var result=document.getElementById("change1")
+    result.style.backgroundColor = 'red';
+}
+function green(){
+    var result=document.getElementById("change2")
+    result.style.backgroundColor = 'green';
+    
+}
+function yellow(){
+    var result=document.getElementById("change3")
+    result.style.backgroundColor = 'yellow';
+}
+
+
+
+
+
+
+
+
+
+function changeColor(divId, color) {
+    var result = document.getElementById(divId);
+    result.style.backgroundColor = color;
+}
